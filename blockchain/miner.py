c310a248b98008b5a3308472a80532689cf51b02
Nate Magera <nmagera07@gmail.com>
2019-12-06 13:21:17 -0500

    working on problems

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -24,7 +24,9 @@ def proof_of_work(last_proof):
 
     print("Searching for next proof")
     proof = 0
-    #  TODO: Your code here
+    
+    while valid_proof(last_proof, proof) is False:
+        proof += 1
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
@@ -40,7 +42,13 @@ def valid_proof(last_hash, proof):
     """
 
     # TODO: Your code here!
-    pass
+    guess = f"{proof}".encode()
+    last_hash = f"{last_hash}".encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+
+    last_guess = hashlib.sha256(last_hash).hexdigest()
+
+    return str(last_guess)[-6:] == str(guess_hash)[:6]
 
 
 if __name__ == '__main__':
